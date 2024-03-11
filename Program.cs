61f272ca3f5c6783ec8fa6f527a12855202dcbb4
ALI SHAIKH 1973 <139847421+alishaikh1973@users.noreply.github.com>
2024-03-11 03:48:24 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using BookCRUD.Data;
+using BookCRUD.Repositories;
+using BookCRUD.Services;
+using Microsoft.EntityFrameworkCore;
+
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+
+            var connectionString = builder.Configuration.GetConnectionString("defaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(op => op.UseSqlServer(connectionString));
+
+
+            // inject the services in Project
+
+            builder.Services.AddScoped<IBookRepo,BookRepo>();
+            builder.Services.AddScoped<IBookService,BookService>();
+
+
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
