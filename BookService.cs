4064b9994a2a0102326a9c95c10ee5b6563ff8db
ALI SHAIKH 1973 <139847421+alishaikh1973@users.noreply.github.com>
2024-03-11 03:52:26 -0700

    Add files via upload

---
+++ BookService.cs
@@ -0,0 +1,38 @@
+﻿using BookCRUD.Models;
+using BookCRUD.Repositories;
+
+namespace BookCRUD.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepo repo;
+        public BookService(IBookRepo repo) 
+        {
+            this.repo=repo;
+        }
+        public int AddBook(Book book)
+        {
+           return repo.AddBook(book);
+        }
+
+        public int DeleteBook(int id)
+        {
+            return repo.DeleteBook(id);
+        }
+
+        public Book GetBookById(int id)
+        {
+            return repo.GetBookById(id);
+        }
+
+        public IEnumerable<Book> GetBooks()
+        {
+            return repo.GetBooks();
+        }
+
+        public int UpdateBook(Book book)
+        {
+            return repo.UpdateBook(book);
+        }
+    }
+}
---
+++ IBookService.cs
@@ -0,0 +1,14 @@
+﻿using BookCRUD.Models;
+
+namespace BookCRUD.Services
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetBooks();
+        Book GetBookById(int id);
+        int AddBook(Book book); 
+        int UpdateBook(Book book);
+        int DeleteBook(int id);
+
+    }
+}
